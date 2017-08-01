fa00640ac96cce03025964626ac75aee3590cde8
spartrekus <spartrekus@gmail.com>
2017-08-01 11:40:54 +0200

    Add files via upload

---
+++ hello.c
@@ -0,0 +1,20 @@
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("- TERMINAL4PI -\n");
+    printf("Hello World \n");
+    return 0;
+}
+
+
+
+
+
+
+
+
+
---
+++ hellocode.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+	printf( "    #include <stdio.h>\n" );
+	printf( "    int main()\n" );
+	printf( "    {\n" );
+	printf( "	printf(\"Hello World \\n\");\n" );
+	printf( "	return 0;\n" );
+       printf( "    }\n" );
+       return 0;
+}
+
+
+
+
+
