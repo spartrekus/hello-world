f8806046f41bbcdc1cb075a53721503bc205afe0
spartrekus <spartrekus@gmail.com>
2018-05-21 15:42:47 +0200

    Add files via upload

---
+++ hello-world-gt.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()                                                                                                    
+{
+   do
+   {
+     puts("Hello World\n");
+   } while (getchar() != EOF);
+ 
+   return 0;                                                                                            
+}
